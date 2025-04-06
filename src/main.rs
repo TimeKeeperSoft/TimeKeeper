c5812d1ca810caab7037a2f7fea1cdf95a4fd28d
mskrasnov <linuxoid85@gmail.com>
2025-04-06 13:23:29 +0300

    Clean code; add docs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
+//! TimeKeeper is the simplest cross-platform program for PC time tracking.
+//! It is used to periodically remind the user of the need to take breaks
+//! while working on a PC.
+//!
+//! ## Idea
+//! Many people do not take breaks when working at a PC. As a result, the
+//! efficiency of such work decreases due to fatigue, musculoskeletal
+//! problems due to sedentary lifestyle and all kinds of eye disorders.
+//! Consequently, users need a simple program that, adjusting to their
+//! rhythm of work, could remind them of the need to take a break from work.
+
 #![windows_subsystem = "windows"]
 
-pub mod conf;
+mod conf;
 pub mod consts;
-pub mod stats;
-pub mod time;
-pub mod traits;
-pub mod ui;
+mod stats;
+mod time;
+mod traits;
+mod ui;
 
 fn main() -> iced::Result {
     ui::ui()
