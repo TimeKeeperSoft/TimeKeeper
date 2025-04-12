86db8dd47c14e5ebfa34dea5ed40f00045d396a9
mskrasnov <linuxoid85@gmail.com>
2025-04-12 15:09:41 +0300

    The size of the small font now depends on the OS in which the program is running

--- src/ui/view/about.rs
+++ src/ui/view/about.rs
@@ -18,6 +18,15 @@ use iced::{
     },
 };
 
+/* Since in Linux the 12th font was too large, and therefore the window did
+ * not fit the “OK” button, we change this value to 10. In Windows, the 12th
+ * font is also quite comfortable.
+ */
+#[cfg(windows)]
+const SMALL_TEXT_SIZE: u16 = 12;
+#[cfg(unix)]
+const SMALL_TEXT_SIZE: u16 = 10;
+
 impl TimeKeeper {
     pub fn about_page(&self) -> Element<Message> {
         let about_devs = column![
@@ -25,15 +34,15 @@ impl TimeKeeper {
                 row![text("Идея и реализация"), horizontal_rule(0)]
                     .spacing(5)
                     .align_y(Center),
-                text("Михаил Краснов <https://github.com/mskrasnov>").size(12),
+                text("Михаил Краснов <https://github.com/mskrasnov>").size(SMALL_TEXT_SIZE),
             ]
             .spacing(3),
             column![
                 row![text("Другие участники"), horizontal_rule(0)]
                     .spacing(5)
                     .align_y(Center),
-                text("Данила Макаров: дизайн, текст проекта").size(12),
-                text("Максим Марушин: тестирование, текст проекта").size(12),
+                text("Данила Макаров: дизайн, текст проекта").size(SMALL_TEXT_SIZE),
+                text("Максим Марушин: тестирование, текст проекта").size(SMALL_TEXT_SIZE),
             ]
             .spacing(3),
             column![
@@ -42,11 +51,11 @@ impl TimeKeeper {
                     .align_y(Center),
                 row![
                     url_button("Сайт", PROG_SITE).on_press(Message::OpenSiteUrl),
-                    text("|").size(12),
+                    text("|").size(SMALL_TEXT_SIZE),
                     url_button("Репозиторий", PROG_REPO).on_press(Message::OpenRepoUrl),
-                    text("|").size(12),
+                    text("|").size(SMALL_TEXT_SIZE),
                     url_button("crates.io", PROG_CRATES_URL).on_press(Message::OpenCratesUrl),
-                    text("|").size(12),
+                    text("|").size(SMALL_TEXT_SIZE),
                     url_button("Telegram", PROG_TELEGRAM).on_press(Message::OpenTelegramUrl),
                 ]
                 .spacing(5),
